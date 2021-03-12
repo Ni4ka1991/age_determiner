e33cc8fae9e24dd4190a22ca385e7d498d99407f
Ni4ka <astahova_nica@mail.ru>
2021-03-12 21:20:11 +0200

    Add file age_determiner.py

---
+++ age_determiner.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env Python3
+
+#Data
+
+year_of_birth = int(input("Please, enter your date of birth:\n"))
+
+
+
+#logic
+
+if (1900 < year_of_birth <= 2021):
+ if (year_of_birth =  ): 
+else:
+ print("Your entered is outside the date range")
+
+
+
