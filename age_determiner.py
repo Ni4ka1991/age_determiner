002e8a7eed36910ec3f7058423b52efd0f38e5e6
Ni4ka <astahova_nica@mail.ru>
2021-03-13 11:40:33 +0200

    Add file age_determiner.py

--- age_determiner.py
+++ age_determiner.py
@@ -2,16 +2,32 @@
 
 #Data
 
-year_of_birth = int(input("Please, enter your date of birth:\n"))
+year_of_birth = int(input("\nPlease, enter your date of birth:\n"))
 
 
 
 #logic
 
-if (1900 < year_of_birth <= 2021):
- if (year_of_birth =  ): 
+if (1900 < year_of_birth <= 2021):            #Data checking
+ 
+ age = 2021 - year_of_birth                   #Age calculation
+ 
+ #Age-range classification
+ if ( 1 <= age <= 3 ):
+  print ( "Baby" )
+ elif ( 4 <= age < 9 ):
+  print ( "Kid" )
+ elif (  10 <= age <= 15 ):
+  print ( "Teen ")
+ elif ( 16 <= age <= 18 ):
+  print ( "Young" )
+ elif ( 19 <= age <= 50 ):
+  print ( "Adult" )
+ elif ( age >= 51 ):
+  print ("Old")
+ 
 else:
- print("Your entered is outside the date range")
+ print("\nYour entered is outside the date range")
 
 
 
